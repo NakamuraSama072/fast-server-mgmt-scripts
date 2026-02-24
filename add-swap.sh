5fd7ffe208daa39a4d2480a587e94767ba6c67f4
NakamuraSama072 <stephanwilliamson@qq.com>
2026-02-24 14:10:14 +0800

    feat: Add initial scripts for swap file creation and admin user setup
    - They are only placeholders since some unrecoverable issues occurred in my KDE Discover

---
+++ add-swap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "To be added: Add swap file creation and configuration steps here"
---
+++ create-admin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "To be added: Creating admin user and granting sudo privileges, disabling SSH password authentication for root"
